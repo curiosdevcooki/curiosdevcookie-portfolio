93f2581317548e6f5ca996dce9d26a621ea88d76
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2024-03-01 05:34:52 +0100

    Revert "Refactor mobileSVG for CLI function."
    This reverts commit 8d6d388de3ae2217d60c9adf8a28d9d8e60c283c.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   createAndInsertSVG();
-  removeMobileSVGforCLIprojects(["mobileSVG-figure-ascii-art"]);
   addImagesToSVG();
 
+  removeCertainMobileSVGs("mobileSVG-figure-ascii-art");
 
 });
 
@@ -162,6 +162,7 @@ function addClassesToImages(mockup, projectID) {
 }
 
 function addImagesToSVG() {
+
   const projectIDs = getIDofLiElements();
   projectIDs.forEach((projectID) => {
     const desktopImage = document.querySelector(`.desktop-image.${projectID}`);
@@ -174,13 +175,16 @@ function addImagesToSVG() {
   });
 }
 
-function removeMobileSVGforCLIprojects(listOfIDs) {
-  const idsToRemove = new Set(listOfIDs.map(id => `${id}`));
 
-  const mobileSVGs = document.querySelectorAll('[id*="mobileSVG-"]');
-  mobileSVGs.forEach(svg => {
-    if (idsToRemove.has(svg.id)) {
-      svg.remove();
+function removeCertainMobileSVGs(id) {
+
+  const mobileSVGs = document.querySelectorAll(`[id*="mobileSVG-"]`);
+  mobileSVGs.forEach(el => {
+    if (el.id == id) {
+      el.remove();
     }
   });
 }
+
+
+
